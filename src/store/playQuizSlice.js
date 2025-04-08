0bd858892b02f59eec1183ec2ed49af39bc899fc
oscar <oscar.burman005@gmail.com>
2025-04-08 17:47:39 +0200

    Sessin storing current question to prevent app from crashing if page is refreshed while playing the quiz

--- src/store/playQuizSlice.js
+++ src/store/playQuizSlice.js
@@ -4,14 +4,15 @@ export const USERNAME_KEY = "quizAppUsername";
 export const CODE_KEY = "quizAppCode";
 const SCORE_KEY = "quizAppScore";
 const CURRENT_QUESTIONS_KEY = "quizAppCurrentQuestion";
+const QUIZ_NAME_KEY = "quizAppQuizname";
 
 const initialState = {
   connected: false,
   username: localStorage.getItem(USERNAME_KEY) || "",
   code: sessionStorage.getItem(CODE_KEY) || "",
-  score: sessionStorage.getItem(SCORE_KEY) || "",
-  currentQuestion: sessionStorage.getItem(CURRENT_QUESTIONS_KEY) || "",
-  quizName: "",
+  score: JSON.parse(sessionStorage.getItem(SCORE_KEY) || "0"),
+  currentQuestion: JSON.parse(sessionStorage.getItem(CURRENT_QUESTIONS_KEY) || "null"),
+  quizName: sessionStorage.getItem(QUIZ_NAME_KEY) || "",
   error: null,
 };
 
@@ -38,15 +39,17 @@ const playQuizSlice = createSlice({
     setScore: (state, action) => {
       const score = action.payload;
       state.score = score;
-      sessionStorage.setItem(SCORE_KEY, score);
+      sessionStorage.setItem(SCORE_KEY, JSON.stringify(score));
     },
     setCurrentQuestion: (state, action) => {
       const currentQuestion = action.payload;
       state.currentQuestion = currentQuestion;
-      sessionStorage.setItem(CURRENT_QUESTIONS_KEY, currentQuestion);
+      sessionStorage.setItem(CURRENT_QUESTIONS_KEY, JSON.stringify(currentQuestion));
     },
     setQuizName: (state, action) => {
-      state.quizName = action.payload;
+      const quizName = action.payload;
+      state.quizName = quizName;
+      sessionStorage.setItem(QUIZ_NAME_KEY, quizName);
     },
     setError: (state, action) => {
       state.error = action.payload;
