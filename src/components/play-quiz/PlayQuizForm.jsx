a891d9863527e8b17129639e735bf18e1c35f685
oscar <oscar.burman005@gmail.com>
2025-04-10 19:25:52 +0200

    Options shuffle logic

--- src/components/play-quiz/PlayQuizForm.jsx
+++ src/components/play-quiz/PlayQuizForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { OPTION_COLORS } from "../create-quiz/QuizForm";
 import usePlayQuizStore from "../../hooks/usePlayQuizStore";
+import { shuffleArray } from "../../utils/helpers";
 
 export default function PlayQuizForm({ options = [], setHasAnswered }) {
   const { sendMessage } = usePlayQuizStore();
@@ -8,25 +9,32 @@ export default function PlayQuizForm({ options = [], setHasAnswered }) {
   const onSubmit = (e) => {
     e.preventDefault();
     const selectedValue = e.nativeEvent.submitter.value;
-    sendMessage({ type: "ANSWER_QUESTION", answer: selectedValue });
+
+    const index = options.findIndex((option) => option === selectedValue);
+
+    sendMessage({ type: "ANSWER_QUESTION", answer: index });
     setHasAnswered(true);
   };
+
+  const shuffledOptions = shuffleArray(options);
 
   return (
     <form onSubmit={onSubmit}>
       <ul className="grid grid-cols-1fr md:grid-cols-[1fr_1fr] gap-8 p-8 md:mt-32">
-        {options.map((option, index) => {
+        {shuffledOptions.map((option, index) => {
           return (
             <li key={index}>
               <button
                 type="submit"
-                value={index}
+                value={option}
                 style={{
                   backgroundColor: OPTION_COLORS[index],
                   boxShadow: "inset 0 0 20px 10px rgba(0, 0, 0, 0.5)",
                 }}
                 className="min-h-36 h-full w-full rounded-xl transition-all duration-300 ease hover:opacity-70 cursor-pointer">
-                <p className="text-xl text-black font-medium">{option}</p>
+                <p className="w-[90%] mx-auto text-xl text-black text-center font-medium">
+                  {option}
+                </p>
               </button>
             </li>
           );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -56,3 +56,15 @@ export function safeParseJSON(str) {
     return null;
   }
 }
+
+export function shuffleArray(array) {
+  const suffledArray = [...array];
+
+  for (let i = suffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [suffledArray[i], suffledArray[j]] = [suffledArray[j], suffledArray[i]];
+  }
+
+  return suffledArray;
+}
